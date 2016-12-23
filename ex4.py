a8b5c9b6b9aeeae504738045c0af09c0ab539f0f
wenyan666 <wenyan.zhang@foxmail.com>
2016-12-23 13:43:49 +0800

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,17 @@
+# -*-coding:UTF-8 -*-
+# 给出的已知条件
+cars = 100
+space_in_a_car=4.0
+drivers=30
+passengers=90
+cars_not_driven=cars-drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers/cars_driven
+# 根据上面给出的已知条件来计算其他的一些数据
+print "There are",cars,"cars abailable."
+print "There are only",drivers,"drivers available."
+print "There will be",cars_not_driven,"empty cars today."
+print "We can transport",carpool_capacity,"people today."
+print "We have",passengers,"to carpool today."
+print "We need to put about",average_passengers_per_car,"in each car."
