1a28c851430d7d01351bfda3b95deeddf764c990
wenyan666 <wenyan.zhang@foxmail.com>
2016-12-23 13:58:29 +0800

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,10 +1,10 @@
 # -*-coding:UTF-8 -*-
 # 给出的已知条件
 cars = 100
-space_in_a_car=4.0
-drivers=30
-passengers=90
-cars_not_driven=cars-drivers
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars-drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers/cars_driven
