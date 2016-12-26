c381f15f866d40031eb7f9ca6c8a05158152cccf
wenyan666 <wenyan.zhang@foxmail.com>
2016-12-26 18:25:02 +0800

    Update ex05.py
    回头再看一遍，复习了转义符，现在对转义符认识更深入了。并增加了加分练习。

--- ex05.py
+++ ex05.py
@@ -15,5 +15,16 @@ print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (my_eyss,my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
 
+# 加分题4 1英寸=2.54厘米 1磅=0.4536千克
+
+my_height_centimeter = my_height*2.54
+my_weight_kelomrtre = my_weight*0.4536
+
+print "Let's talk about you."
+your_name = raw_input("type your name:")
+print "Hi,%r!" % your_name
+print "You know what? I'm %.2f centermeters tall. And I'm %.2f kilometres heavy." % (my_height_centimeter, my_weight_kelomrtre)
+
+
 # this line is tricky,try to get it exactly tight
 print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
