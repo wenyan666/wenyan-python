b9a5aa0d9a3c1b17a9ffbdd085894446657b7ed4
wenyan666 <wenyan.zhang@foxmail.com>
2016-12-27 16:18:12 +0800

    Update ex08.py

--- ex08.py
+++ ex08.py
@@ -1,3 +1,4 @@
+# -*- coding:UTF-8 -*-
 formatter = "%r %r %r %r"
 print formatter % (1,2,3,4)
 print formatter % ("one", "two", "three", "four")
@@ -9,3 +10,10 @@ print formatter % (
    "But it didn't sing.",
    "So I said goodnight."
 )
+# 最后一行既有单引号，又有双引号，发现把don't中的‘去掉之后就显示单引号了。可能是这个原因。
+print '''
+I learn some new thing in ex9,so I back here to do some excise.
+I wanna know what happed if I use three quotes.
+Is it the same with three double-quetes?
+I will see.
+'''
