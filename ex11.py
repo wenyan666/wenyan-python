2b852ce722d1e96c8d7b373056c0fb4720f4935d
wenyan666 <wenyan.zhang@foxmail.com>
2016-12-27 16:32:51 +0800

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -1,3 +1,4 @@
+# -*- coding:UTF-8 -*-
 print "How old are you?",
 age = raw_input()
 print "How tall are you?",
@@ -7,3 +8,7 @@ weight = raw_input()
 
 print "So,you're %r old, %r tall and %r heavy." %(age,height,weight)
 
+# raw_input：读取一行输入的字符串
+# 其他用法
+job = raw_input("What\'s your work for?")
+# 在输入6'25"的时候为嘛后面的"不用转义字符呢？
