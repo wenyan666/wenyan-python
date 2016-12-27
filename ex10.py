c3d7a0a4ecfb915f9e98616c0c1874d506d43e39
wenyan666 <wenyan.zhang@foxmail.com>
2016-12-27 16:20:09 +0800

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -14,8 +14,15 @@ print persian_cat
 print backslash_cat
 print fat_cat
 
+# 加分习题3：将转义序列和格式化字符串组合到一起，创建一种更复杂的格式。 
+a = 6
+b = 480
+c = "yeah!"
+print "How about \'%r\'/\'%r\'?" % (b, a),b/a
+print "%s That\'s coll!" %c
+
 # 附加练习
 while True:
    for i in ["/","-","|","\\","|"]:
       print "%s\r" % i,
-   
+# 这个还挺神奇的，一直转个不停，也没有办法结束，这是啥？
