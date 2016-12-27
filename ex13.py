796564e8e48eb85ccc504014139e0c853ebdf388
wenyan666 <wenyan.zhang@foxmail.com>
2016-12-27 17:55:27 +0800

    Update ex13.py

--- ex13.py
+++ ex13.py
@@ -1,8 +1,20 @@
+# -*- coding:UTF-8 -*-
 from sys import argv
 
 script, first, second, third = argv
 
-print "The script is called:",script
-print "Your first variable is:",first
-print "Your second variable is:", second
-print "Your thied variable is:", third
+
+print "Your script is called:" , script
+print "Your first variable is: " , first
+print "Your second variable is:" , second
+print "Your third variable is:" , third
+
+# 这里的参数变量都用逗号连接，改成用%s还有效么？已测。可以！
+# 这里第一行输入的为什么显示的文件名？原理是什么？  日后再解决
+
+script, one, two, three = argv
+
+print "test:%s" % script
+print "test:%s" % one
+print "test:%s" % two
+print "test:%s" %three
