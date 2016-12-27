762cc7d184ee842110a8f4189afc04d825e71ab0
wenyan666 <wenyan.zhang@foxmail.com>
2016-12-27 16:18:37 +0800

    Update ex08.py

--- ex08.py
+++ ex08.py
@@ -1,19 +1,16 @@
 # -*- coding:UTF-8 -*-
-formatter = "%r %r %r %r"
-print formatter % (1,2,3,4)
-print formatter % ("one", "two", "three", "four")
-print formatter % (True, False, False, True)
-print formatter % (formatter, formatter, formatter, formatter)
-print formatter % (
-   "I had this thing.",
-   "That you could type up right.",
-   "But it didn't sing.",
-   "So I said goodnight."
-)
-# 最后一行既有单引号，又有双引号，发现把don't中的‘去掉之后就显示单引号了。可能是这个原因。
-print '''
-I learn some new thing in ex9,so I back here to do some excise.
-I wanna know what happed if I use three quotes.
-Is it the same with three double-quetes?
-I will see.
-'''
+# Here's some new strange stuff, remember type it exactly.
+# 用\n可以换行
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# 这里是用逗号连接了字符串，是不是用加号也可以？
+# 是的，确实可以！
+print "Here are the days: ", days
+print "Here are the months: " + months
+
+print """
+There's something going on here.
+With the there double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or5, or 6.
+"""
