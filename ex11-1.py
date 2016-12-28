346189c2df01dca3462fce3711dec38da8478461
wenyan666 <wenyan.zhang@foxmail.com>
2016-12-28 16:11:25 +0800

    Delete ex11-1.py

--- ex11-1.py
+++
@@ -1,5 +0,0 @@
-mumber = input("How many people in your family?")
-height = input("How tall are you?(cm):\n")
-weight = input("How much do you weigh?(kg):\n")
-
-print "So,you're %r old, %r tall and %r heavy." %(mumber,height,weight)
