6d5e3cf49dcabb8a476f809fe1bd6e4046dd9294
wenyan666 <wenyan.zhang@foxmail.com>
2016-12-28 10:42:57 +0800

    Update ex14.py

--- ex14.py
+++ ex14.py
@@ -1,10 +1,11 @@
 # -*- coding:UTF-8 -*-
 from sys import argv
+# 加分练习3，给脚本多加了一个变量funny
+script, user_name, funny = argv
+# 加分练习2，修改prompt的内容运行
+prompt = "type here >"
 
-script, user_name = argv
-prompt = ">"
-
-print "Hi,%s, I\'m %s script." %(user_name, script)
+print "Hi,%s, I\'m %s script.I\'m %s to meet you." %(user_name, script, funny)
 print "I will ask you few questions."
 print "Do you like me?" , user_name
 likes = raw_input(prompt)
@@ -18,7 +19,7 @@ computer = raw_input(prompt)
 print "OK,%s,you said %s about like me." % (user_name, likes)
 print "You live %s, sure don't know where is here." % lives
 print "you said you have %s computer.that's coll!" % computer
-# 默写的，和原书中略有不同。但是可以正常运行起来。不错。下面是原书中的代码
+# 默写的，和原书中的内容和方法不同。但是可以正常运行起来。
 print '''
 Alright, so you said %r about liking me.
 You live in %r. Not sure where that is.
